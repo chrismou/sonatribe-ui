2122bbc97902887488f69b7c84614181dab815fb
wayne-o <wayne@prjatk.com>
2015-02-01 17:10:06 +0000

    fixing link-to listing event

--- app/routes/listing-event.js
+++ app/routes/listing-event.js
@@ -3,9 +3,9 @@ import Ajax from 'sonatribe-ui/mixins/sonatribe-ajax';
 
 export default Ember.Route.extend(Ajax, {
 	model: function(params){
-		return this.store.find('listingEvent', {slug: params.Slug})
+		return this.store.find('listingevent', {slug: params.Slug})
 				.then(function(result){
-					 var res = result.get('firstObject'); 
+					 var res = result.get('firstObject');
 					 return res;
 				});
 	}
